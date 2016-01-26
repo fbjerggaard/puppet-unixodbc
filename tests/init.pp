233011de434f514e1f28d0136d5d2429aebc6943
Frederik Bjerggaard Nielsen <fbn@firstcom.dk>
2016-01-26 15:17:22 +0100

    Adding test

--- tests/init.pp
+++ tests/init.pp
@@ -10,3 +10,28 @@
 # http://docs.puppetlabs.com/guides/tests_smoke.html
 #
 include unixodbc
+
+unixodbc::resource::server { '192.168.1.1':
+  ensure    => 'present',
+  driver    => 'PostgreSQL',
+  database  => 'myawesomestuff',
+  fileUsage => 5432,
+}
+
+unixodbc::resource::driver { 'PostgreSQL':
+  ensure      => 'present',
+  description => 'ODBC for PostgreSQL',
+  driver      => '/usr/lib/psqlodbc.so',
+  setup       => '/usr/lib/libodbcpsqlS.so',
+  driver64    => '/usr/lib64/psqlodbc.so',
+  setup64     => '/usr/lib64/libodbcpsqlS.so',
+  fileUsage   => 1,
+}
+
+unixodbc::resource::driver { 'FreeTDS':
+  ensure      => 'present',
+  description => 'ODBC for SQL Server',
+  driver      => '/usr/lib64/libtdsodbc.so.0',
+  setup       => '/usr/lib64/libtdsS.so',
+  fileUsage   => 1,
+}
